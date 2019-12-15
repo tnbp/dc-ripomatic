ec963fb2a930c7673ca7cea8eb4e448ea66f63f4
tnbp <m_105@gmx.de>
2019-12-15 16:30:57 +0100

    Attempt to transcode if VIDEO_TS directory exists
    ffmpeg may fail for some DVDs, attempt to rip anyway if VIDEO_TS directory is present

--- ripomatic.sh
+++ ripomatic.sh
@@ -17,18 +17,18 @@ if [ "$is_dvd" -gt 1 ]; then
         is_video_dvd=$(mplayer -identify -frames 0 /dev/sr0 2>&1 | grep VIDEO: | grep MPEG2 | wc -c)
         if [ "$is_video_dvd" -le 1 ]; then
                 is_video_dvd=$(ffmpeg -i /dev/sr0 2>&1 | grep dvd_nav_packet | wc -c)
+                if [ "$is_video_dvd" -le 1 ]; then
+                        echo "Uuh... I can't tell if it is a video DVD..."
+                        is_video_dvd=$(ls /mnt/sr0/VIDEO_TS | wc -l)
+                        if [ "$is_video_dvd" -gt 0 ]; then
+                                echo "But it seems to have the files for a video DVD, so... I'll try my best! >:3"
+                                is_video_dvd="99"
+                        else
+                                echo "That's not a video DVD!"
+                        fi
+                fi
         fi
-fi
-if [ "$is_video_dvd" -gt 1 ]; then
-        echo "That's a video DVD!"
-        targetdir="/mnt/servervol/ripomatic/Video/$(date +%s)-dvd"
-        mkdir -p "$targetdir"
-        makemkvcon --decrypt --minlength=1000 mkv disc:0 all "$targetdir"
-        chmod 0777 "$targetdir"
-        exit $?
-else
-        echo "That's not a video DVD!"
-fi
+if
 
 is_bluray=$(dvd+rw-mediainfo /dev/sr0 2>&1 | grep BD-ROM | wc -c)
 if [ "$is_bluray" -gt 1 ]; then
